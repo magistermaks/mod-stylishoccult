45ce3b2a616f412dbd6bdf780f3013fc4f13f68f
magistermaks <magistermaks@darktree.net>
2021-03-09 16:31:25 +0100

    Improved spark rendering

--- src/main/java/net/darktree/stylishoccult/entities/SparkEntityRenderer.java
+++ src/main/java/net/darktree/stylishoccult/entities/SparkEntityRenderer.java
@@ -30,32 +30,40 @@ public class SparkEntityRenderer extends EntityRenderer<SparkEntity> {
     }
 
     public void render(SparkEntity entity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
-        PlayerEntity player = MinecraftClient.getInstance().player;
+        float w = (float) Math.sin( entity.age / 10.0f ) * 0.1f + 0.4f;
 
-        if (player != null) {
-            matrixStack.push();
-            matrixStack.scale(0.5F, 0.5F, 0.5F);
-            matrixStack.translate(0, 0.3, 0);
-            matrixStack.multiply(this.dispatcher.getRotation());
-            matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
-            MatrixStack.Entry entry = matrixStack.peek();
-            Matrix4f matrix4f = entry.getModel();
-            Matrix3f matrix3f = entry.getNormal();
-            VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(LAYER);
+        matrixStack.push();
+        matrixStack.scale(w, w, w);
+        matrixStack.translate(0, 0.3, 0);
+        matrixStack.multiply(this.dispatcher.getRotation());
+        matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
 
-            vertex(vertexConsumer, matrix4f, matrix3f, i, 0.0F, 0, 0, 1);
-            vertex(vertexConsumer, matrix4f, matrix3f, i, 1.0F, 0, 1, 1);
-            vertex(vertexConsumer, matrix4f, matrix3f, i, 1.0F, 1, 1, 0);
-            vertex(vertexConsumer, matrix4f, matrix3f, i, 0.0F, 1, 0, 0);
+        MatrixStack.Entry entry = matrixStack.peek();
+        Matrix4f matrix4f = entry.getModel();
+        Matrix3f matrix3f = entry.getNormal();
+        VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(LAYER);
 
-            matrixStack.pop();
-            super.render(entity, f, g, matrixStack, vertexConsumerProvider, i);
-        }
+        vertex(vertexConsumer, matrix4f, matrix3f, 0.0F, 0, 0, 1);
+        vertex(vertexConsumer, matrix4f, matrix3f, 1.0F, 0, 1, 1);
+        vertex(vertexConsumer, matrix4f, matrix3f, 1.0F, 1, 1, 0);
+        vertex(vertexConsumer, matrix4f, matrix3f, 0.0F, 1, 0, 0);
 
+        matrixStack.pop();
+        super.render(entity, f, g, matrixStack, vertexConsumerProvider, i);
     }
 
-    private static void vertex(VertexConsumer vertexConsumer, Matrix4f matrix4f, Matrix3f matrix3f, int i, float f, int j, int k, int l) {
-        vertexConsumer.vertex(matrix4f, f - 0.5F, (float)j - 0.5F, 0.0F).color(255, 255, 255, 255).texture((float)k, (float)l).overlay(OverlayTexture.DEFAULT_UV).light(i).normal(matrix3f, 0.0F, 1.0F, 0.0F).next();
+    /**
+     * @see EntityRenderDispatcher drawFireVertex, for the origin of the '240' magic light value.
+     */
+    private static void vertex(VertexConsumer vertexConsumer, Matrix4f matrix4f, Matrix3f matrix3f, float f, int j, int k, int l) {
+        vertexConsumer
+                .vertex(matrix4f, f - 0.5F, j - 0.5F, 0.0F)
+                .color(255, 255, 255, 255)
+                .texture(k, l)
+                .overlay(OverlayTexture.DEFAULT_UV)
+                .light(240)
+                .normal(matrix3f, 0.0F, 1.0F, 0.0F)
+                .next();
     }
 
     public Identifier getTexture(SparkEntity leashKnotEntity) {
